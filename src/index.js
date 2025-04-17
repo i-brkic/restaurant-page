70d8776a574c57b5858262515b879d108ec18bb0
i-brkic <184224468+i-brkic@users.noreply.github.com>
2025-04-17 14:36:24 +0200

    Add working tab navigation
    Add working tab navigation for home, menu and contact with working tab
    navigation.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@ import loadMenu from "./menu";
 import loadContact from "./contact";
 
 loadHome();
-loadMenu();
-loadContact();
+
+const content = document.querySelector("#content");
+const buttonHome = document.querySelector("#home");
+const buttonMenu = document.querySelector("#menu");
+const buttonContact = document.querySelector("#contact");
+
+buttonHome.addEventListener("click", () => {
+  content.innerHTML = ``;
+  loadHome();
+});
+
+buttonMenu.addEventListener("click", () => {
+  content.innerHTML = ``;
+  loadMenu();
+});
+
+buttonContact.addEventListener("click", () => {
+  content.innerHTML = ``;
+  loadContact();
+});
